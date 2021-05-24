64e450fddab55794c326f1638b559f9cc3089dd8
Adem Altan <adem.altan@purple.telstra.com>
2021-05-24 20:31:36 +1000

    first commit

---
+++ Points/ClientApp/src/components/Points.js
@@ -0,0 +1,107 @@
+﻿import React, { Component } from 'react';
+import Plot from "react-plotly.js";
+import { Button, Badge } from 'react-bootstrap';
+import { Stats } from './Stats';
+
+const initialState = {
+    x: [],
+    y: [],
+    inside: 0,
+    outside: 0
+};
+
+export class Points extends Component {
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+        this.generatePoints = this.generatePoints.bind(this);
+    }
+    counter = 0;
+
+    generatePoints() {
+        this.counter++;
+        this.setState(initialState);
+        let xArray = [];
+        let yArray = [];
+
+        for (let i = 0; i < 100; i++) {
+            let x = Math.floor((Math.random() * 1000));
+            let y = Math.floor((Math.random() * 1000));
+            xArray.push(x);
+            yArray.push(y);
+            this.isWithinRadiusOfCenter(x, y);
+        }
+        this.setState({
+            x: xArray,
+            y: yArray
+        });
+    }
+
+    isWithinRadiusOfCenter(x, y, gridSize = 1000, radius = 450) {
+        let centre = gridSize / 2;
+        let distance = Math.sqrt(Math.pow(centre - x, 2) + Math.pow(centre - y, 2));
+        if (distance < radius) {
+            this.setState(prevState => {
+                return { inside: prevState.inside + 1 }
+            });
+        } else {
+            this.setState(prevState => {
+                return { outside: prevState.outside + 1 }
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Stats
+                    trial={this.counter}
+                    inside={this.state.inside}
+                    outside={this.state.outside}
+                /><br />
+                <p>
+                    <Button variant="outline-primary" size="lg" onClick={this.generatePoints}>Let there be points</Button>{' '}
+                </p>
+                <Plot
+                    data={[
+                        {
+                            x: this.state.x,
+                            y: this.state.y,
+                            type: 'scatter',
+                            mode: 'markers',
+                            marker: { color: 'purple' }
+                        },
+                        
+                    ]}
+                    layout={{ width: '100%', height: 1000, title: 'Points' },
+                    {
+                        title: 'Points',
+                        xaxis: {
+                            range: [0, 1000],
+                            zeroline: false
+                        },
+                        yaxis: {
+                            range: [0, 1000]
+                        },
+                        width: 500,
+                        height: 500,
+                        shapes: [
+                            {
+                                type: 'circle',
+                                x0: 50,
+                                y0: 50,
+                                x1: 950,
+                                y1: 950,
+                                line: {
+                                    color: 'rgba(50, 171, 96, 1)'
+                                }
+                            }
+                        ]
+                    }}
+                    config={{ displaylogo: false }}
+                />
+                
+            </div>
+        )
+    }
+}
---
+++ Points/ClientApp/src/components/Stats.js
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+import { Toast, Badge } from 'react-bootstrap';
+
+const initialState = {
+    inside: 0,
+    outside: 0,
+    ratio: 0
+};
+
+export class Stats extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inside: 0,
+            outside: 0,
+            ratio: 0,
+            trial: 0
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Toast>
+                    <Toast.Header>
+                        <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                        <strong className="mr-auto">Stats</strong>
+                        <small>Trial: {this.props.trial}</small>
+                    </Toast.Header>
+                    <Toast.Body>
+                        <p>Inside: {this.props.inside}</p>
+                        <p>Outside: {this.props.outside}</p>
+                        <p>Ratio: {this.props.ratio}</p>
+                    </Toast.Body>
+                </Toast>
+            </div>
+        )
+    }
+}
