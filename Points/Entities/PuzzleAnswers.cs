45ec087e472931179cc2e7d15ef04eaf2de27b87
Adem Altan <adem.altan@purple.telstra.com>
2021-06-13 23:33:35 +1000

    Adding answers class

---
+++ Points/Entities/PuzzleAnswers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Points.Entities
+{
+    public class PuzzleAnswers
+    {
+        public int[] X { get; set; }
+        public int[] Y { get; set; }
+        public int NumberOfPointsInside { get; set; }
+        public int NumberOfPointsOutside { get; set; }
+        public double RatioOfPointsInside { get; set; }
+        public double EstimatedPi { get; set; }
+    }
+}
